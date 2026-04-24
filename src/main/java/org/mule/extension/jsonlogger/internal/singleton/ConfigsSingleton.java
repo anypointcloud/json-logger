da507ad969e391119c70eb558f87c56eff20bf72
Matthias Transier <matthias.transier@gmail.com>
2026-04-24 19:08:00 +0200

    Trigger the initialisation of the config if it cannot be found
    This can happen in rare exceptional cases in MUnit tests when no
    JSONLogger operation is part of the tested flows. The change is kept
    minimal to avoid any consequences in the behavior for the standard case.

--- src/main/java/org/mule/extension/jsonlogger/internal/singleton/ConfigsSingleton.java
+++ src/main/java/org/mule/extension/jsonlogger/internal/singleton/ConfigsSingleton.java
@@ -1,20 +1,27 @@
 package org.mule.extension.jsonlogger.internal.singleton;
 
 import org.mule.extension.jsonlogger.internal.JsonloggerConfiguration;
+import org.mule.runtime.api.artifact.Registry;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 public class ConfigsSingleton {
 
+    @Inject
+    private Registry registry;
+    
     private Map<String, JsonloggerConfiguration> configs = new HashMap<String, JsonloggerConfiguration>();
 
-    public Map<String, JsonloggerConfiguration> getConfigs() {
-        return configs;
-    }
-
     public JsonloggerConfiguration getConfig(String configName) {
-        return this.configs.get(configName);
+        JsonloggerConfiguration config = configs.get(configName);
+        if (config == null) {
+            registry.lookupByName(configName);
+            config = configs.get(configName);
+        }
+        return config;
     }
 
     public void addConfig(String configName, JsonloggerConfiguration config) {
